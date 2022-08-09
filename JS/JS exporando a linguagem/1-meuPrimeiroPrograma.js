18a0284ab8c77add5492be85f1d46eb080ec9224
Vinicius Gabriel <88984052+VManock@users.noreply.github.com>
2022-08-08 21:42:51 -0300

    Add files via upload

--- JS/JS exporando a linguagem/1-meuPrimeiroPrograma.js
+++ JS/JS exporando a linguagem/1-meuPrimeiroPrograma.js
@@ -1,1 +1,12 @@
-document.body += "Hello World";
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+
+const idade = 22;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
