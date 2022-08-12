924a6db7fc10d65900ee7c53c64fadd09dcf2332
Vinicius Gabriel <88984052+VManock@users.noreply.github.com>
2022-08-11 23:11:30 -0300

    Add files via upload

--- JS/JS exporando a linguagem/7-condicionais.js
+++ JS/JS exporando a linguagem/7-condicionais.js
@@ -6,19 +6,37 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 15;
-const estaAcompanhada = true;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 console.log("Destinos possívei:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1); //removendo item
-} else if (estaAcompanhada) {
-    console.log("Está acompanhado");
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else if (estaAcompanhada) {
+//     console.log("Está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Comprado com sucesso"); 
     listaDeDestinos.splice(1, 1); //removendo item
 } else {
-    console.log("Não é maior de idade e não posso vender");
+    console.log("Não é maior de idade e não está acompanhado, não posso vender");
 }
+
+console.log("Embargue: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem");
+}else{
+    console.log("Não pode embarcar")
+
+}
+
+
 
 // alt shift f -> formata o código
 
