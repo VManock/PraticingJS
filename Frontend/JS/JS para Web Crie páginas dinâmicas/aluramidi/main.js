10f53a53c8d0b64b6029c740cc0ab0e2bfd0dadd
Vinicius Gabriel <88984052+VManock@users.noreply.github.com>
2022-09-16 21:42:31 -0300

    Add files via upload

--- Frontend/JS/JS para Web Crie páginas dinâmicas/aluramidi/main.js
+++ Frontend/JS/JS para Web Crie páginas dinâmicas/aluramidi/main.js
@@ -1,22 +1,33 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){    
+         elemento.play();
+    
+    } else {
+        console.log('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0; 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
-  
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; 
 
-    const idAudio = `#som_${instrumento}`;
-
-    tecla.onclick = function(){
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
+    
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+             tecla.classList.add('ativa');
+        }
+    }
 
-    contador = (contador + 1);
-
+    tecla.onkeyup = function () {
+            tecla.classList.remove('ativa');
+    }
 }
