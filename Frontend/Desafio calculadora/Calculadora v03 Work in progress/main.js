bf9e60db9a6a53667a78d268325c288f4bd89ef4
Vinicius Gabriel <88984052+VManock@users.noreply.github.com>
2022-09-29 23:16:44 -0300

    Add files via upload

--- Frontend/Desafio calculadora/Calculadora v03 Work in progress/main.js
+++ Frontend/Desafio calculadora/Calculadora v03 Work in progress/main.js
@@ -1,6 +1,31 @@
 let display = document.getElementById('display');
 
+let memoria = document.getElementById('memoria');
+
+let operador = document.getElementById('operador');
+
 let buttons = Array.from(document.getElementsByClassName('button'));
+
+// function checarUltimoDigito(ultimoDigito){
+//     if((ultimoDigito == '-') || (ultimoDigito == '+') || (ultimoDigito == '/') || (ultimoDigito == '*') || (ultimoDigito == '.')){
+//         return true;
+//     }
+//     return false;
+// }
+
+// function numeroSemVirgula(innerText){
+//     var ultimoNumero = getUltimoNumero(innerText);
+//     if(ultimoNumero.includes('.')){
+//         return false;
+//     }
+//     return true;
+// }
+
+// function getUltimoNumero(innerText){
+//     var inicioDoUltimoNumero = innerText.indexOf('+');
+//     var ultimoNumero = innerText.slice(inicioDoUltimoNumero, innerText.lenght);
+//     return ultimoNumero;
+// }
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
@@ -10,14 +35,98 @@ buttons.map( button => {
                 display.innerText = '';
                 break;
 
-            case '⬅': 
+            case '←': 
                 if(display.innerText){
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
             
+            case '+':
+                if(display.innerText){
+                    if(memoria.innerText){
+                        memoria.innerText = parseInt(memoria.innerText) + parseInt(display.innerText);
+                        operador.innerText = '+';
+                        display.innerText = '';
+                        console.log("operação realizada")
+                    } else {
+                    memoria.innerText = display.innerText;
+                    operador.innerText = '+';
+                    display.innerText = '';
+                    console.log("display não nulo")
+                    }
+                } else {
+                    operador.innerText = '+';
+                    console.log("display nulo")
+                }
+                break;
+
+            case '-':
+                if(display.innerText){
+                    if(memoria.innerText){
+                        memoria.innerText = parseInt(memoria.innerText) - parseInt(display.innerText);
+                        operador.innerText = '-';
+                        display.innerText = '';
+                        console.log("operação realizada")
+                    } else {
+                    memoria.innerText = display.innerText;
+                    operador.innerText = '-';
+                    display.innerText = '';
+                    console.log("display não nulo")
+                    }
+                } else {
+                    operador.innerText = '-';
+                    console.log("display nulo")
+                }
+                break;
+
+            case '/':
+                if(display.innerText){
+                    if(memoria.innerText){
+                        memoria.innerText = parseInt(memoria.innerText) / parseInt(display.innerText);
+                        operador.innerText = '/';
+                        display.innerText = '';
+                        console.log("operação realizada")
+                    } else {
+                    memoria.innerText = display.innerText;
+                    operador.innerText = '/';
+                    display.innerText = '';
+                    console.log("display não nulo")
+                    }
+                } else {
+                    operador.innerText = '/';
+                    console.log("display nulo")
+                }
+                break;  
+            
+            case '*':
+                if(display.innerText){
+                    if(memoria.innerText){
+                        memoria.innerText = parseInt(memoria.innerText) * parseInt(display.innerText);
+                        operador.innerText = '*';
+                        display.innerText = '';
+                        console.log("operação realizada")
+                    } else {
+                    memoria.innerText = display.innerText;
+                    operador.innerText = '*';
+                    display.innerText = '';
+                    console.log("display não nulo")
+                    }
+                } else {
+                    operador.innerText = '*';
+                    console.log("display nulo")
+                }
+                break;
+        
+            case '.':
+                //checar se existe ponto no display, caso não existe, colocar, caso já exista, não fazer nada
+            break;
+                
+            case '=':
+
+            break;
+                
             default:
                 display.innerText += e.target.innerText;
         }
     })
-});
+})
