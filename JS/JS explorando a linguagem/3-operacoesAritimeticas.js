0ca302e1a5d0e6fe5bf677836b5d75f3bdadef09
Vinicius Gabriel <88984052+VManock@users.noreply.github.com>
2022-10-06 14:32:14 -0300

    Add files via upload

---
+++ JS/JS explorando a linguagem/3-operacoesAritimeticas.js
@@ -0,0 +1,12 @@
+console.log("Operações Aritiméticas");
+
+console.log(2+2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+const ano = 2022;
+console.log(ano + 2020); 
+
+console.log ("ano" + 2020);
+console.log ("2" + "2");
+
---
+++ JS/JS manipulando o DOM/Exercicios/Data-attribute/script.js
@@ -0,0 +1,11 @@
+function mostraTintas (botao) {
+    if (botao === "vazio"){
+    document.getElementById('lista').style.opacity = 0
+    } else {
+    document.getElementById('lista').style.opacity = 100
+
+    }
+}
+
+// WIP
+
