65502ecf1fdae037362c6a26836dcdb4fce1e131
Vinicius Gabriel <88984052+deviniciusg@users.noreply.github.com>
2024-01-18 16:59:46 -0300

    Add files via upload

--- Curso Lari/Calculadora 2024/script.js
+++ Curso Lari/Calculadora 2024/script.js
@@ -1,1 +1,6 @@
-console.log("Hello, Harold!")
+function adicionar_numero (numero) {
+    var operacao = document.getElementById("operacao");
+    var conteudo_da_operacao = operacao.innerHTML;
+
+    var numero_atual = descobrir
+}
