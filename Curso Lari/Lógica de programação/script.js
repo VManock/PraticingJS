bbdc00764cfcb55da56f31b47a25de7f5be222f5
Lariisssa <larissaro310@gmail.com>
2024-02-28 21:44:53 -0300

    sugestoes de melhorias na organizacao do js

--- Curso Lari/Lógica de programação/script.js
+++ Curso Lari/Lógica de programação/script.js
@@ -1,3 +1,44 @@
+var numeralExercicio = parseInt(prompt("Selecione o exercício desejado utilizando o numeral correspondente (1, 2, 3, 4 ou 5).\n\n1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B e mostre se a soma é menor que C.\n\n2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.\n\n3 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores, caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e imprimir seu valor na tela.\n\n4 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.\n\n5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20)."))
+
+if (numeralExercicio == 1) {
+    exercicio1()
+} else if (numeralExercicio == 2) {
+    console.log("WIP")
+} else if (numeralExercicio == 3) {
+    console.log("WIP")
+}else if (numeralExercicio == 4) {
+    console.log("WIP")
+}else if (numeralExercicio == 5) {
+    console.log("WIP")
+}else if (numeralExercicio != Number) {
+    alert('Isso não é um número, por favor selecione um número')
+}else {
+    alert("Erro! Reinicia a página")
+}
+
+selecionaNovoExercicio()
+
+function comparaABC (a,b,c) {
+    if (a + b < c) {
+        var soma = a + b
+        return console.log(`Resultado do exercício 1: A soma de a+b é ${soma} o que é menor que o valor de c (${c})`)
+    } else if (a + b == c) {
+        return console.log(`Resultado do exercício 1: A soma de a (${a}) + b (${b}) é igual a c (${c})`)
+    } else if (a + b > c) {
+        return console.log("Resultado do exercício 1: A soma de a + b foi maior que c.")
+    } else {
+        return console.log("Erro")
+    }
+}
+
+function exercicio1() {
+    var a = parseInt(prompt("Você selecionou o exercicio 1, insira o valor de 'a'"))
+    var b = parseInt(prompt("Obrigado, agora insira o valor de 'b'"))
+    var c = parseInt(prompt("Obrigado, agora insira o valor de 'c'"))
+    
+    comparaABC(a, b, c)
+}
+
 function selecionaNovoExercicio() {
     var numeralExercicio = parseInt(prompt("Pronto! O resultado do exercício anterior está no console\n Selecione um novo exercício desejado utilizando o numeral correspondente (1, 2, 3, 4 ou 5).\n\n1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B e mostre se a soma é menor que C.\n\n2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.\n\n3 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores, caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e imprimir seu valor na tela.\n\n4 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.\n\n5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20)."))
 
@@ -21,42 +62,11 @@ function selecionaNovoExercicio() {
     else {alert("Erro")}
 } 
 
-var numeralExercicio = parseInt(prompt("Selecione o exercício desejado utilizando o numeral correspondente (1, 2, 3, 4 ou 5).\n\n1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B e mostre se a soma é menor que C.\n\n2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.\n\n3 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores, caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e imprimir seu valor na tela.\n\n4 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.\n\n5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20)."))
 
-if (numeralExercicio == 1) {
-    var a = parseInt(prompt("Você selecionou o exercicio 1, insira o valor de 'a'"))
-    var b = parseInt(prompt("Obrigado, agora insira o valor de 'b'"))
-    var c = parseInt(prompt("Obrigado, agora insira o valor de 'c'"))
-    
-    comparaABC(a,b,c)
-
-    selecionaNovoExercicio()
-} 
-
-    else if (numeralExercicio == 2) {console.log(WIP)}
-    else if (numeralExercicio == 3) {console.log(WIP)}
-    else if (numeralExercicio == 4) {console.log(WIP)}
-    else if (numeralExercicio == 5) {console.log(WIP)}
-    else if (numeralExercicio != Number) {alert('Isso não é um número, por favor selecione um número')}
-    else {alert("Erro! Reinicia a página")}
-
-
-
-function comparaABC (a,b,c) {
-   if (a+b<c){ 
-        var soma = a+b
-        return console.log(`Resultado do exercício 1: A soma de a+b é ${soma} o que é menor que o valor de c (${c})`)
-    } else if (a+b==c) {
-        return console.log(`Resultado do exercício 1: A soma de a (${a}) + b (${b}) é igual a c (${c})`)
-    } else if (a+b>c) {
-        return console.log("Resultado do exercício 1: A soma de a + b foi maior que c.")
-    } else {
-    return console.log("Erro")
-}
 
 
     
-}
+
 
 
 // EXERCÍCIO 12 - Faça um algoritmo que leia o valor de um produto e determine o valor que deve ser pago, conforme a escolha da forma de pagamento
